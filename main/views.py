3d06239664531019cd71b1f336e0f849372fc16c
glebmiller <gleb.miller.ge@gmail.com>
2022-10-10 18:31:56 +0400

    4.2 complete, views added

--- main/views.py
+++ main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from main.models import User, Task, Tag
+from serializers import UserSerializer, TaskSerializer, TagSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.order_by("id")
+    serializer_class = UserSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.order_by("id")
+    serializer_class = TaskSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.order_by("id")
+    serializer_class = TagSerializer
