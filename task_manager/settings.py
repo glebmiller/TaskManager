aec0ba41e225d334fa316490cf26c1e30cfeec75
glebmiller <gleb.miller.ge@gmail.com>
2022-10-11 19:11:53 +0400

    4.3 + 4.4 completed

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -130,3 +130,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "main.User"
 
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+}
